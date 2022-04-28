42a7a34c2b0db206c73675868608f068c2114ae9
Roni <rampage9198@yahoo.com>
2022-04-28 15:23:55 +0700

    Initial commit

---
+++ app/src/main/java/com/gamecodeschool/material/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.gamecodeschool.material
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.widget.doOnTextChanged
+import com.gamecodeschool.material.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.textInputLayout3.setEndIconOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.textInputEditText4.doOnTextChanged { text, start, before, count ->
+            if (text!!.length>10){
+                binding.textInputLayout4.error = "no More!"
+            }else if (text!!.length<10){
+                binding.textInputLayout4.error = null
+            }
+            }
+
+//        val iconclick = findViewById<TextInputLayout>(R.id.textInputEditText3)
+//        iconclick.setEndIconOnClickListener{
+//            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+//        }
+    }
+}
