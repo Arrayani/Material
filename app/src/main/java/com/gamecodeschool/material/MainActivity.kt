ea3611653efa6bb88d57a01516347e788180b37d
Roni <rampage9198@yahoo.com>
2022-07-08 16:39:24 +0700

    commit

--- app/src/main/java/com/gamecodeschool/material/MainActivity.kt
+++ app/src/main/java/com/gamecodeschool/material/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.gamecodeschool.material
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.widget.doOnTextChanged
 import com.gamecodeschool.material.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputLayout
@@ -25,14 +25,20 @@ class MainActivity : AppCompatActivity() {
         binding.textInputEditText4.doOnTextChanged { text, start, before, count ->
             if (text!!.length>10){
                 binding.textInputLayout4.error = "no More!"
-            }else if (text!!.length<10){
+            }else if (text.length<10){
                 binding.textInputLayout4.error = null
             }
             }
+
+        binding.secondBtn.setOnClickListener{
+            val intent = Intent(this,SecondPage::class.java)
+            startActivity(intent)
+        }
 
 //        val iconclick = findViewById<TextInputLayout>(R.id.textInputEditText3)
 //        iconclick.setEndIconOnClickListener{
 //            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
 //        }
     }
+
 }
